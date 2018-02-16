7e69f95ce3c19ecb570d07333d63dbdeceac16ae
Soumit <soumitkumar4@gmail.com>
2018-02-17 01:36:54 +0600

    Version 1.1

---
+++ app/src/main/java/com/example/soumit/booksearch/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.soumit.booksearch.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.soumit.booksearch.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
